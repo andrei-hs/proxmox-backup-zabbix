3dcb1631bec370cca43d9ff964af291e08feae7a
andrei-hs <123700875+andrei-hs@users.noreply.github.com>
2026-05-05 14:34:38 -0300

    Fix: Update backup-status.sh to use zabbix_agentd configuration

--- backup-status.sh
+++ backup-status.sh
@@ -4,35 +4,31 @@
 #
 #17/09/25 - Andrei Henrique Santos
 
-ZABBIX_SERVER=$(awk -F= '/^ServerActive=/{print $2}' /etc/zabbix/zabbix_agentd.conf | tail -n 1)
-
-ZABBIX_SERVER_IP=$(echo "$ZABBIX_SERVER" | cut -d':' -f1)
-ZABBIX_SERVER_PORT=$(echo "$ZABBIX_SERVER" | cut -d':' -f2)
-ZABBIX_NODE=$(zabbix_agentd -t system.hostname | awk -F'[|\\]]' '{print $2}')
 ZABBIX_KEY="proxmox.backup"
+ZABBIX_CONF_FILE="/etc/zabbix/zabbix_agentd.conf"
+ZABBIX_NODE=$(zabbix_agentd -t system.hostname | awk -F'[|\\]]' '{print $2}')
 
 phase=$1
 vmid=$3
 
 sendToZabbix() {
-	local send_to=$1
-	local port=$2
-	local host=$3
-	local key=$4
-	local data=$5
+	local config_file=$1
+	local host=$2
+	local key=$3
+	local data=$4
 
-	zabbix_sender -z "$send_to" -p "$port" -s "$host" -k "$key" -o "$data"
+	zabbix_sender -c "$config_file" -s "$host" -k "$key" -o "$data"
 }
 
 case "$phase" in
 	"backup-end")
-		sendToZabbix "$ZABBIX_SERVER_IP" "$ZABBIX_SERVER_PORT" "$ZABBIX_NODE" "$ZABBIX_KEY.status[$ZABBIX_NODE,$vmid,$STOREID]" 1
+		sendToZabbix "$ZABBIX_CONF_FILE" "$ZABBIX_NODE" "$ZABBIX_KEY.status[$ZABBIX_NODE,$vmid,$STOREID]" 1
 		;;
 	"backup-abort")
-		sendToZabbix "$ZABBIX_SERVER_IP" "$ZABBIX_SERVER_PORT" "$ZABBIX_NODE" "$ZABBIX_KEY.status[$ZABBIX_NODE,$vmid,$STOREID]" 0
+		sendToZabbix "$ZABBIX_CONF_FILE" "$ZABBIX_NODE" "$ZABBIX_KEY.status[$ZABBIX_NODE,$vmid,$STOREID]" 0
 		;;
 	"log-end" | "log-abort")
 		log=$(<"$LOGFILE")
-		sendToZabbix "$ZABBIX_SERVER_IP" "$ZABBIX_SERVER_PORT" "$ZABBIX_NODE" "$ZABBIX_KEY.log[$ZABBIX_NODE,$vmid,$STOREID]" "$log"
+		sendToZabbix "$ZABBIX_CONF_FILE" "$ZABBIX_NODE" "$ZABBIX_KEY.log[$ZABBIX_NODE,$vmid,$STOREID]" "$log"
 		;;
 esac
